58a30d2b536fda086685f9d76481360ee98e36d2
Kyle Franklin <kpfranklin22@gmail.com>
2023-03-23 21:29:23 -0400

    updating project - prettier ext- added README

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# project-pokedex
+# <h1 align="center"> Project Pokédex
+
+[![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)](https://opensource.org/licenses/ISC)
+
+## Table of Contents
+
+- [Description](#description)
+- [Installation](#installation)
+- [Contributions](#contributions)
+- [Contact](#contact)
+- [License](#license)
+
+## Description
+
+This is an application using HTML, CSS, JavaScript, and the free PokéAPI to emulate the handheld Pokémon identification technology depicted in the 90s animated TV show, Pokémon.
+
+## Installation
+
+Packages required: This is a vanilla JavaScript application, it doesn't require any package installation.
+
+## Contributions
+
+Kyle Franklin - CSS provided by Justin Kim
+
+## Contact
+
+Email: kpfranklin22@gmail.com <br/>
+Github: [Kpfranklin22](https://github.com/Kpfranklin22)
+
+## License
+
+ISC
+
+## <h2 align="center"> :octocat: README created with [Pro-Readme-Generator](https://github.com/Kpfranklin22/pro-readme-generator) :octocat:
--- app.js
+++ app.js
@@ -1,49 +1,49 @@
 // DOM Objects
-const mainScreen = document.querySelector(".main-screen");
-const pokeName = document.querySelector(".poke-name");
-const pokeId = document.querySelector(".poke-id");
-const pokeFrontImage = document.querySelector(".poke-front-image");
-const pokeBackImage = document.querySelector(".poke-back-image");
-const pokeTypeOne = document.querySelector(".poke-type-one");
-const pokeTypeTwo = document.querySelector(".poke-type-two");
-const pokeWeight = document.querySelector(".poke-weight");
-const pokeHeight = document.querySelector(".poke-height");
-const pokeListItems = document.querySelectorAll(".list-item");
-const leftButton = document.querySelector(".left-button");
-const rightButton = document.querySelector(".right-button");
+const mainScreen = document.querySelector('.main-screen');
+const pokeName = document.querySelector('.poke-name');
+const pokeId = document.querySelector('.poke-id');
+const pokeFrontImage = document.querySelector('.poke-front-image');
+const pokeBackImage = document.querySelector('.poke-back-image');
+const pokeTypeOne = document.querySelector('.poke-type-one');
+const pokeTypeTwo = document.querySelector('.poke-type-two');
+const pokeWeight = document.querySelector('.poke-weight');
+const pokeHeight = document.querySelector('.poke-height');
+const pokeListItems = document.querySelectorAll('.list-item');
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
 
 // constants and variables
 
 const TYPES = [
-  "normal",
-  "fighting",
-  "flying",
-  "poison",
-  "ground",
-  "rock",
-  "bug",
-  "ghost",
-  "steel",
-  "fire",
-  "water",
-  "grass",
-  "electric",
-  "psychic",
-  "ice",
-  "dragon",
-  "dark",
-  "fairy",
+  'normal',
+  'fighting',
+  'flying',
+  'poison',
+  'ground',
+  'rock',
+  'bug',
+  'ghost',
+  'steel',
+  'fire',
+  'water',
+  'grass',
+  'electric',
+  'psychic',
+  'ice',
+  'dragon',
+  'dark',
+  'fairy',
 ];
 
-let prevUrl = "null";
-let nextUrl = "null";
+let prevUrl = 'null';
+let nextUrl = 'null';
 
 // Functions
 
 const capitalize = (str) => str[0].toUpperCase() + str.substr(1);
 
 const resetScreen = () => {
-  mainScreen.classList.remove("hide");
+  mainScreen.classList.remove('hide');
 
   for (const type of TYPES) {
     mainScreen.classList.remove(type);
@@ -64,11 +64,11 @@ const fetchPokeList = (url) => {
 
         if (resultData) {
           const { name, url } = resultData;
-          const urlArray = url.split("/");
+          const urlArray = url.split('/');
           const id = urlArray[urlArray.length - 2];
-          pokeListItem.textContent = id + ". " + capitalize(name);
+          pokeListItem.textContent = id + '. ' + capitalize(name);
         } else {
-          pokeListItem.textContent = "";
+          pokeListItem.textContent = '';
         }
       }
     });
@@ -80,25 +80,25 @@ const fetchPokeData = (id) => {
     .then((data) => {
       resetScreen();
 
-      const dataTypes = data["types"];
+      const dataTypes = data['types'];
       const dataFirstType = dataTypes[0];
       const dataSecondType = dataTypes[1];
-      pokeTypeOne.textContent = capitalize(dataFirstType["type"]["name"]);
+      pokeTypeOne.textContent = capitalize(dataFirstType['type']['name']);
       if (dataSecondType) {
-        pokeTypeTwo.classList.remove("hide");
-        pokeTypeTwo.textContent = capitalize(dataSecondType["type"]["name"]);
+        pokeTypeTwo.classList.remove('hide');
+        pokeTypeTwo.textContent = capitalize(dataSecondType['type']['name']);
       } else {
-        pokeTypeTwo.classList.add("hide");
-        pokeTypeTwo.textContent = "";
+        pokeTypeTwo.classList.add('hide');
+        pokeTypeTwo.textContent = '';
       }
-      mainScreen.classList.add(dataFirstType["type"]["name"]);
-      pokeName.textContent = capitalize(data["name"]);
-      pokeId.textContent = "#" + data["id"].toString().padStart(3, "0");
-      pokeHeight.textContent = data["height"];
-      pokeWeight.textContent = data["weight"];
+      mainScreen.classList.add(dataFirstType['type']['name']);
+      pokeName.textContent = capitalize(data['name']);
+      pokeId.textContent = '#' + data['id'].toString().padStart(3, '0');
+      pokeHeight.textContent = data['height'];
+      pokeWeight.textContent = data['weight'];
 
-      pokeFrontImage.src = data["sprites"]["front_default"] || "";
-      pokeBackImage.src = data["sprites"]["back_default"] || "";
+      pokeFrontImage.src = data['sprites']['front_default'] || '';
+      pokeBackImage.src = data['sprites']['back_default'] || '';
     });
 };
 
@@ -119,16 +119,16 @@ const handleListItemClick = (e) => {
   const listItem = e.target;
   if (!listItem.textContent) return;
 
-  const id = listItem.textContent.split(".")[0];
+  const id = listItem.textContent.split('.')[0];
   fetchPokeData(id);
 };
 
 // Adding event listeners
-leftButton.addEventListener("click", handleLeftButtonClick);
-rightButton.addEventListener("click", handleRightButtonClick);
+leftButton.addEventListener('click', handleLeftButtonClick);
+rightButton.addEventListener('click', handleRightButtonClick);
 for (const pokeListItem of pokeListItems) {
-  pokeListItem.addEventListener("click", handleListItemClick);
+  pokeListItem.addEventListener('click', handleListItemClick);
 }
 //initialize app
 
-fetchPokeList("https://pokeapi.co/api/v2/pokemon?offset=0&limit=20");
+fetchPokeList('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20');
